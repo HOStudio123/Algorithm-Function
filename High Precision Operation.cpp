1eb668061fde895d37316a1f2a018d0261c34797
HOStudio123 <hostudio.hopybox@foxmail.com>
2024-10-01 17:49:25 +0800

    update High Precision Operation.cpp

--- High Precision Operation.cpp
+++ High Precision Operation.cpp
@@ -1,18 +1,51 @@
 #include <iostream>
 #include <string>
-#define maxn 200000
+#define maxn 600000
+
 using namespace std;
-int a[maxn], b[maxn], c[maxn];
-int main()
+
+string plus_jf(string A, string B)
 {
-    string A, B;
-    cin >> A >> B;
+    string C;
+    int a[maxn] = {0}, b[maxn] = {0}, c[maxn] = {0};
     int l = max(A.length(), B.length());
-    int sign = 0;
+    for (int i = A.length() - 1, j = 1; i >= 0; i--, j++)
+    {
+        a[j] = A[i] - '0';
+    }
+    for (int i = B.length() - 1, j = 1; i >= 0; i--, j++)
+    {
+        b[j] = B[i] - '0';
+    }
+    for (int i = 1; i <= l; i++)
+    {
+        c[i] += a[i] + b[i];
+        c[i + 1] = c[i] / 10;
+        c[i] %= 10;
+    }
+    if (c[l + 1])
+        l++;
+    for (int i = l; i >= 1; i--)
+    {
+        C += c[i] + '0';
+    }
+    return C;
+}
+
+string minus_jf(string A, string B)
+{
+    string C;
+    int a[maxn] = {0}, b[maxn] = {0}, c[maxn] = {0};
+    int l = max(A.length(), B.length());
+    int sign = 0, k = 0;
     if (A.length() < B.length())
+    {
         sign = 1;
+    }
     else if (A < B && A.length() == B.length())
+    {
         sign = 1;
+    }
     for (int i = A.length() - 1, j = 1; i >= 0; i--, j++)
     {
         a[j] = A[i] - '0';
@@ -37,18 +70,27 @@ int main()
             c[i] += 10;
         }
     }
-    int k = 0;
     if (sign == 1)
-        cout << "-";
+    {
+        C += "-";
+    }
     for (int i = l; i >= 1; i--)
     {
         if (c[i] != 0 || k != 0)
         {
-            cout << c[i];
+            C += c[i] + '0';
             k = 1;
         }
     }
-    if (k==0){
-        cout<<0;
+    if (k == 0)
+    {
+        C = "0";
     }
+    return C;
+}
+int main()
+{
+    string A, B;
+    cin >> A >> B;
+    cout << plus_jf(A, B) << end << minus_jf(A, B);
 }
